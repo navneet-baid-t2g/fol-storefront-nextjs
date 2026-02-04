49537d5088428564977b1764072696d43e2d3e40
Ishita <rathiishita01@gmail.com>
2026-02-04 17:57:12 +0530

    Exception domain for images

--- next.config.js
+++ next.config.js
@@ -18,44 +18,14 @@ const nextConfig = {
       fullUrl: true,
     },
   },
-  images: {
-    domains: ['api.fols.in'],
-  },
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "http",
-  //       hostname: "localhost",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "medusa-server-testing.s3.amazonaws.com",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
-  //     },
-  //     ...(S3_HOSTNAME && S3_PATHNAME
-  //       ? [
-  //           {
-  //             protocol: "https",
-  //             hostname: S3_HOSTNAME,
-  //             pathname: S3_PATHNAME,
-  //           },
-  //         ]
-  //       : []),
-  //   ],
-  // },
+
   images: {
   remotePatterns: [
     {
@@ -63,6 +33,11 @@ const nextConfig = {
       hostname: "localhost",
       port: "9000",
       pathname: "/static/**",
+    },
+    {
+      protocol: 'https',
+      hostname: 'api.fols.in',
+      pathname: '/static/**',
     },
     {
       protocol: "http",
