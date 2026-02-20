5d585c3331c5e721d25299c318e0e270639bf349
Ishita <rathiishita01@gmail.com>
2026-02-20 16:36:14 +0530

    Product image issue fix

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,7 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || "https://api.fols.in";
+export const BACKEND_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
+    : "https://api.fols.in";
 export const STRAPI_BASE_URL = process.env.NEXT_PUBLIC_STRAPI_API_BASE_URL!;
 export const BANNER_SLIDES = [
     "/images/banner-1.png",
